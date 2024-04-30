ec2174b5b73eee75e205e9c1b3f39d21ece4c7f5
Danielle Biá <daniellebiaviana@gmail.com>
2024-04-30 08:26:34 -0300

    segundo commit

--- src/ParametrosInvalidosException.java
+++ src/ParametrosInvalidosException.java
@@ -1,6 +1,5 @@
-public class ParametrosInvalidosException{
-    public static void main(String[] args){
-        System.out.println("juba");
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(){
+        super("Parâmetros inválidos! O segundo parâmetro deve ser maior que o primeiro parâmetro.");
     }
-
 }
